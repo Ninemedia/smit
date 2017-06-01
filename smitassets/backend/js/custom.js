c9e62eda1beb1f15d7ade78fbf025feec35133c9
radenrifal <radenrifalardiansyah@gmail.com>
2017-06-01 23:03:47 +0700

    Update Backend

--- smitassets/backend/js/custom.js
+++ smitassets/backend/js/custom.js
@@ -187,6 +187,49 @@ $("body").delegate( "a.workunitdelete", "click", function( event ) {
     });
 });
 
+// News Delete
+$("body").delegate( "button.newsdelete", "click", function( event ) {
+    event.preventDefault();
+    var url = $(this).attr('href');
+    var table_container = $('#workunit_list').parents('.dataTables_wrapper');
+    var msg = '';
+    
+    bootbox.confirm("Anda yakin akan menghapus data berita ini?", function(result) {
+        if( result == true ){
+            $.ajax({
+                type:   "POST",
+                url:    url,
+                beforeSend: function (){
+                    $("div.page-loader-wrapper").fadeIn();
+                },
+                success: function( response ){                    
+                    $("div.page-loader-wrapper").fadeOut();
+                    response = $.parseJSON(response);
+                    
+                    if( response.msg == 'error' ){
+                        App.alert({
+                            type: 'danger', 
+                            icon: 'warning', 
+                            message: response.message, 
+                            container: table_container, 
+                            place: 'prepend'
+                        });
+                    }else{
+                        App.alert({
+                            type: 'success', 
+                            icon: 'check', 
+                            message: response.message, 
+                            container: table_container, 
+                            place: 'prepend'
+                        });
+                    }
+                    $('#btn_workunit_list').trigger('click');
+                }
+            });
+        }
+    });
+});
+
 /*
 $("body").delegate( "a.tenantconfirm", "click", function( event ) {
     event.preventDefault();
