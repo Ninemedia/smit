96597dc821404a7bf691d653821b1302341c6eee
Raden Rifal Ardiansyah <rardiansyah@pukado.lan>
2017-07-03 10:16:31 +0700

    Update

--- smitapp/views/backend/infografis/user.php
+++ smitapp/views/backend/infografis/user.php
@@ -4,9 +4,36 @@
         <div class="card">
             <div class="header"><h2>Info Grafis Pengguna</h2></div>
             <div class="body">
-                <div id="chart-user" style="height: 300px;">
-    				<span class="data hide"><?php echo $chart; ?></span>
-    			</div>
+                <!-- Nav tabs -->
+                <ul class="nav nav-tabs" role="tablist">
+                    <li role="presentation" class="active">
+                        <a href="#year" data-toggle="tab">
+                            <i class="material-icons">donut_small</i> BULANAN
+                        </a>
+                    </li>
+                    <li role="presentation">
+                        <a href="#month" data-toggle="tab">
+                            <i class="material-icons">donut_small</i> TAHUNAN
+                        </a>
+                    </li>
+                </ul>
+
+                <!-- Tab panes -->
+                <div class="tab-content">
+                    <div role="tabpanel" class="tab-pane fade in active" id="year">
+                        <div id="chart-user" style="height: 300px;">
+            				<span class="data hide"><?php echo $chart; ?></span>
+            			</div>
+                    </div><s></s>
+
+                    <div role="tabpanel" class="tab-pane fade" id="month">
+                        <div id="chart-user" style="height: 300px;">
+            				<span class="data hide"><?php echo $chart; ?></span>
+            			</div>
+                    </div>
+                </div>
+
+
             </div>
         </div>
     </div>
--- smitapp/views/frontend/statistic.php
+++ smitapp/views/frontend/statistic.php
@@ -32,9 +32,51 @@
 			</div>
 			<div class="col-md-12">
                 <div class="panel-body">
-                    <p align="justify"></p>
+                    <div class="panel-group" role="tablist" aria-multiselectable="true">
+                        <div class="panel panel-col-blue">
+                            <div class="panel-heading" role="tab" id="heading_praincubation">
+                                <h4 class="panel-title">
+                                    <a role="button" data-toggle="collapse" href="#collapse_praincubation" aria-expanded="true" aria-controls="collapse_praincubation">
+                                        <i class="material-icons">format_align_justify</i> Pra-Inkubasi
+                                    </a>
+                                </h4>
+                            </div>
+                            <div id="collapse_praincubation" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="heading_praincubation">
+                                <div class="panel-body">
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="panel panel-col-blue">
+                            <div class="panel-heading" role="tab" id="heading_incubation">
+                                <h4 class="panel-title">
+                                    <a role="button" data-toggle="collapse" href="#collapse_incubation" aria-expanded="true" aria-controls="collapse_incubation">
+                                        <i class="material-icons">format_align_justify</i> Inkubasi / Tenant
+                                    </a>
+                                </h4>
+                            </div>
+                            <div id="collapse_incubation" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading_incubation">
+                                <div class="panel-body">
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="panel panel-col-blue">
+                            <div class="panel-heading" role="tab" id="heading_ikm">
+                                <h4 class="panel-title">
+                                    <a role="button" data-toggle="collapse" href="#collapse_ikm" aria-expanded="true" aria-controls="collapse_ikm">
+                                        <i class="material-icons">format_align_justify</i> Index Kepuasan Masyarakat
+                                    </a>
+                                </h4>
+                            </div>
+                            <div id="collapse_ikm" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading_ikm">
+                                <div class="panel-body">
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-			    
             </div>
 		</div>
 	</div>
