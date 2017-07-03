f4724065bb3826de2b372667c517af0e8639cb91
Raden Rifal Ardiansyah <rardiansyah@pukado.lan>
2017-07-03 14:07:12 +0700

    Update

--- smitapp/views/backend/services/ikm.php
+++ smitapp/views/backend/services/ikm.php
@@ -4,16 +4,15 @@
         <div class="card">
             <div class="header"><h2>Pengukuran IKM</h2></div>
             <div class="body">
-                <?php if( !empty($is_admin) ) :?>
                 <!-- Nav tabs -->
                 <ul class="nav nav-tabs" role="tablist">
                     <li role="presentation" class="active">
-                        <a href="#list_in" data-toggle="tab">
+                        <a href="#list" data-toggle="tab">
                             <i class="material-icons">list</i> DAFTAR IKM
                         </a>
                     </li>
                     <li role="presentation">
-                        <a href="#list_out" data-toggle="tab">
+                        <a href="#add" data-toggle="tab">
                             <i class="material-icons">add_box</i> TAMBAH IKM
                         </a>
                     </li>
@@ -21,9 +20,9 @@
 
                 <!-- Tab panes -->
                 <div class="tab-content">
-                    <div role="tabpanel" class="tab-pane fade in active" id="list_in">
+                    <div role="tabpanel" class="tab-pane fade in active" id="list">
                         <div class="table-container table-responsive">
-                            <table class="table table-striped table-bordered table-hover" id="communication_listin" data-url="<?php echo base_url('layanan/komunikasibantuan/in'); ?>">
+                            <table class="table table-striped table-bordered table-hover" id="list_ikm" data-url="<?php echo base_url('layanan/komunikasibantuan/in'); ?>">
                                 <thead>
             						<tr role="row" class="heading bg-blue">
             							<th class="width5">No</th>
@@ -68,215 +67,11 @@
                             </table>
                         </div>
                     </div>
-                    <div role="tabpanel" class="tab-pane fade in" id="list_out">
-                        <div class="table-container table-responsive">
-                            <table class="table table-striped table-bordered table-hover" id="communication_listout" data-url="<?php echo base_url('layanan/komunikasibantuan/out'); ?>">
-                                <thead>
-            						<tr role="row" class="heading bg-blue">
-            							<th class="width5">No</th>
-            							<th class="width20 text-center">Nama Pengirim</th>
-            							<th class="width15 text-center">Judul Komunikasi</th>
-            							<th class="width15 text-center">Deskripsi</th>
-            							<th class="width15 text-center">Status</th>
-                                        <th class="width10 text-center">Tanggal Daftar</th>
-            							<th class="width13 text-center">Actions <button class="btn btn-xs btn-warning btn-floating table-search"><i class="material-icons">search</i></button></th>
-       						        </tr>
-                                    <tr role="row" class="filter display-hide table-filter">
-            							<td></td>
-            							<td><input type="text" class="form-control form-filter input-sm text-uppercase" name="search_name" /></td>
-            							<td><input type="text" class="form-control form-filter input-sm text-uppercase" name="search_title" /></td>
-            							<td><input type="text" class="form-control form-filter input-sm text-lowercase" name="search_email" /></td>
-            							<td>
-                                            <select name="search_status" class="form-control form-filter input-sm">
-            									<option value="">Pilih...</option>
-            									<?php
-            			                        	$status = smit_user_status_message();
-            			                            if( !empty($status) ){
-            			                                foreach($status as $key => $val){
-            			                                    echo '<option value="'.$key.'">'.strtoupper($val).'</option>';
-            			                                }
-            			                            }
-            			                        ?>
-            								</select>
-                                        </td>
-                                        <td>
-            								<input type="text" class="form-control form-filter input-sm date-picker text-center bottom5" readonly name="search_datecreated_min" placeholder="From" />
-            								<input type="text" class="form-control form-filter input-sm date-picker text-center" readonly name="search_datecreated_max" placeholder="To" />
-            							</td>
-            							<td style="text-align: center;">
-            								<button class="btn bg-blue waves-effect filter-submit" id="btn_list_user">Search</button>
-                                            <button class="btn bg-red waves-effect filter-cancel">Reset</button>
-            							</td>
-            						</tr>
-                                </thead>
-                                <tbody>
-                                    <!-- Data Will Be Placed Here -->
-                                </tbody>
-                            </table>
-                        </div>
+                    <div role="tabpanel" class="tab-pane fade in" id="add">
+
                     </div>
 
                 </div>
-                <?php else : ?>
-                <!-- Nav tabs -->
-                <ul class="nav nav-tabs" role="tablist">
-                    <li role="presentation" class="active">
-                        <a href="#list_in" data-toggle="tab">
-                            <i class="material-icons">call_received</i> Komunikasi Masuk
-                        </a>
-                    </li>
-                    <li role="presentation">
-                        <a href="#list_out" data-toggle="tab">
-                            <i class="material-icons">call_made</i> Bantuan Keluar
-                        </a>
-                    </li>
-                    <li role="presentation">
-                        <a href="#add" data-toggle="tab">
-                            <i class="material-icons">add_box</i> Buat Komunikasi
-                        </a>
-                    </li>
-                </ul>
-
-                <!-- Tab panes -->
-                <div class="tab-content">
-                    <div role="tabpanel" class="tab-pane fade in active" id="list_in">
-                        <div class="table-container table-responsive">
-                            <table class="table table-striped table-bordered table-hover" id="communication_listin" data-url="<?php echo base_url('layanan/komunikasibantuan/in'); ?>">
-                                <thead>
-            						<tr role="row" class="heading bg-blue">
-            							<th class="width5">No</th>
-            							<th class="width15 text-center">Judul Komunikasi</th>
-            							<th class="width25 text-center">Deskripsi</th>
-            							<th class="width15 text-center">Status</th>
-                                        <th class="width10 text-center">Tanggal Daftar</th>
-            							<th class="width15 text-center">Actions <button class="btn btn-xs btn-warning btn-floating table-search"><i class="material-icons">search</i></button></th>
-       						        </tr>
-                                    <tr role="row" class="filter display-hide table-filter">
-            							<td></td>
-            							<td><input type="text" class="form-control form-filter input-sm text-uppercase" name="search_title" /></td>
-            							<td><input type="text" class="form-control form-filter input-sm text-lowercase" name="search_email" /></td>
-            							<td>
-                                            <select name="search_status" class="form-control form-filter input-sm">
-            									<option value="">Pilih...</option>
-            									<?php
-            			                        	$status = smit_user_status_message();
-            			                            if( !empty($status) ){
-            			                                foreach($status as $key => $val){
-            			                                    echo '<option value="'.$key.'">'.strtoupper($val).'</option>';
-            			                                }
-            			                            }
-            			                        ?>
-            								</select>
-                                        </td>
-                                        <td>
-            								<input type="text" class="form-control form-filter input-sm date-picker text-center bottom5" readonly name="search_datecreated_min" placeholder="From" />
-            								<input type="text" class="form-control form-filter input-sm date-picker text-center" readonly name="search_datecreated_max" placeholder="To" />
-            							</td>
-            							<td style="text-align: center;">
-            								<button class="btn bg-blue waves-effect filter-submit" id="btn_list_user">Search</button>
-                                            <button class="btn bg-red waves-effect filter-cancel">Reset</button>
-            							</td>
-            						</tr>
-                                </thead>
-                                <tbody>
-                                    <!-- Data Will Be Placed Here -->
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                    <div role="tabpanel" class="tab-pane fade in" id="list_out">
-                        <div class="table-container table-responsive">
-                            <table class="table table-striped table-bordered table-hover" id="communication_listout" data-url="<?php echo base_url('layanan/komunikasibantuan/out'); ?>">
-                                <thead>
-            						<tr role="row" class="heading bg-blue">
-            							<th class="width5">No</th>
-            							<th class="width15 text-center">Judul Komunikasi</th>
-            							<th class="width25 text-center">Deskripsi</th>
-            							<th class="width10 text-center">Status</th>
-                                        <th class="width10 text-center">Tanggal Daftar</th>
-            							<th class="width15 text-center">Actions <button class="btn btn-xs btn-warning btn-floating table-search"><i class="material-icons">search</i></button></th>
-       						        </tr>
-                                    <tr role="row" class="filter display-hide table-filter">
-            							<td></td>
-            							<td><input type="text" class="form-control form-filter input-sm text-uppercase" name="search_title" /></td>
-            							<td><input type="text" class="form-control form-filter input-sm text-lowercase" name="search_email" /></td>
-            							<td>
-                                            <select name="search_status" class="form-control form-filter input-sm">
-            									<option value="">Pilih...</option>
-            									<?php
-            			                        	$status = smit_user_status_message();
-            			                            if( !empty($status) ){
-            			                                foreach($status as $key => $val){
-            			                                    echo '<option value="'.$key.'">'.strtoupper($val).'</option>';
-            			                                }
-            			                            }
-            			                        ?>
-            								</select>
-                                        </td>
-                                        <td>
-            								<input type="text" class="form-control form-filter input-sm date-picker text-center bottom5" readonly name="search_datecreated_min" placeholder="From" />
-            								<input type="text" class="form-control form-filter input-sm date-picker text-center" readonly name="search_datecreated_max" placeholder="To" />
-            							</td>
-            							<td style="text-align: center;">
-            								<button class="btn bg-blue waves-effect filter-submit" id="btn_list_user">Search</button>
-                                            <button class="btn bg-red waves-effect filter-cancel">Reset</button>
-            							</td>
-            						</tr>
-                                </thead>
-                                <tbody>
-                                    <!-- Data Will Be Placed Here -->
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                    <div role="tabpanel" class="tab-pane fade" id="add">
-                        <?php echo form_open_multipart( 'backend/communicationadd', array( 'id'=>'cmm_form', 'role'=>'form' ) ); ?>
-                            <div id="alert" class="alert display-hide"></div>
-                            <div class="alert alert-info">
-                                <strong>Perhatian!</strong>
-                                Pesan ini otomatis ditujukan untuk Administrator
-                            </div>
-
-                            <div class="form-group">
-                                <label class="control-label">Judul Komunikasi <b style="color: red !important;">(*)</b></label>
-                                <div class="form-line">
-                                    <?php
-                                        echo form_input(
-                                            'cmm_title',
-                                            ( !empty($post) ? smit_isset($post['cmm_title'],'') : '' ),
-                                            array(
-                                                'class'=>'form-control text-uppercase',
-                                                'id'=>'cmm_title',
-                                                'placeholder'=>'Masukan Judul Komunikasi dan Bantuan...',
-                                                'required'=>'required'
-                                            )
-                                        );
-                                    ?>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label class="control-label">Deskripsi Komunikasi <b style="color: red !important;">(*)</b></label>
-                                <div class="form-line">
-                                    <?php
-                                        echo form_textarea(
-                                            array(
-                                                'name'=>'cmm_description',
-                                                'class'=>'form-control no-resize',
-                                                'id'=>'cmm_description',
-                                                'rows'=>4,
-                                                'placeholder'=>'Silahkan isi deskripsi dari komikasi dan bantuan dengan maksimal 400 huruf...'
-                                            ),
-                                            ( !empty($post) ? smit_isset($post['cmm_description'],'') : '' )
-                                        );
-                                    ?>
-                                </div>
-                            </div>
-                            <button type="submit" class="btn btn-primary waves-effect" id="btn_cmmadd">Tambah Komunikasi</button>
-                            <button type="button" class="btn btn-danger waves-effect" id="btn_cmmadd_reset">Bersihkan</button>
-                        <?php echo form_close(); ?>
-                    </div>
-                </div>
-                <?php endif ?>
             </div>
         </div>
     </div>
--- smitassets/backend/js/pages/table/table-ajax.js
+++ smitassets/backend/js/pages/table/table-ajax.js
@@ -1,5 +1,5 @@
 var TableAjax = function () {
-    
+
     // Init Date Pickers
     var initPickers = function () {
         //Datetimepicker plugin
@@ -15,28 +15,28 @@ var TableAjax = function () {
     var handleRecordsUserList = function() {
         gridTable( $("#user_list"), true );
     };
-    
+
     // -------------------------------------------------------------------------
     // PRAINCUBATION
-    // ------------------------------------------------------------------------- 
+    // -------------------------------------------------------------------------
     // Pra Incubation Selection Lists
     var handleRecordsPraIncubationSelectionList = function() {
         gridTable( $("#praincubation_list"), true );
     };
-    
+
     var handleRecordsPraIncubationSelectionList2 = function() {
         gridTable( $("#praincubation_list2"), true );
     };
-    
+
     var handleRecordsPraIncubationList = function() {
         gridTable( $("#list_praincubation"), true );
     };
-    
+
     // Pra Incubation Selection Setting Lists
     var handleRecordsPraIncubationSettingSelectionList = function() {
         gridTable( $("#praincubation_setting_list"), true );
     };
-    
+
     // Pra Incubation Selection Report Lists
     var handleRecordsPraIncubationReportSelectionList = function() {
         gridTable( $("#praincubationreport_list"), true );
@@ -44,28 +44,28 @@ var TableAjax = function () {
     var handleRecordsPraIncubationReportList = function() {
         gridTable( $("#list_praincubationreport"), true );
     };
-    
-    
+
+
     // Admin Selection Lists Step One
     var handleRecordsAdminStepOneList = function() {
         gridTable( $("#admin_stepone"), true );
     };
-    
+
     // Admin Selection Lists Step Two
     var handleRecordsAdminStepTwoList = function() {
         gridTable( $("#admin_steptwo"), true );
     };
-    
+
     // Admin Score Lists Step One
     var handleRecordsAdminScoreStepOneList = function() {
         gridTable( $("#adminscore_stepone"), true );
     };
-    
+
     // Admin Score Lists Step Two
     var handleRecordsAdminScoreStepTwoList = function() {
         gridTable( $("#adminscore_steptwo"), true );
     };
-    
+
     // Juri History List Pra-Inkubasi
     var handleRecordsJuryHistoryList = function() {
         gridTable( $("#praincubationhistory_list"), true );
@@ -74,144 +74,152 @@ var TableAjax = function () {
     var handleRecordsJuryHistoryIncubationList = function() {
         gridTable( $("#incubationhistory_list"), true );
     };
-    
+
     // Pra Incubation Accompaniment Lists
     var handleRecordsPraIncubationAccompanimentList = function() {
         gridTable( $("#accompaniment_list"), true );
     };
-    
+
     // Pra Incubation Accepted Lists
     var handleRecordsPraIncubationAcceptedList = function() {
         gridTable( $("#acceptedselection_list"), true );
     };
-    
+
     // -------------------------------------------------------------------------
-    
+
     // -------------------------------------------------------------------------
     // INCUBATION
-    // ------------------------------------------------------------------------- 
+    // -------------------------------------------------------------------------
     // Incubation Selection Lists
     var handleRecordsIncubationSelectionList    = function() {
         gridTable( $("#incubation_list"), true );
     };
-    
+
     var handleRecordsIncubationSelectionList2   = function() {
         gridTable( $("#incubation_list2"), true );
     };
-    
+
     // Incubation Selection Setting Lists
     var handleRecordsIncubationSettingSelectionList = function() {
         gridTable( $("#incubation_setting_list"), true );
     };
-    
+
     // Incubation Selection Report Lists
     var handleRecordsIncubationReportSelectionList = function() {
         gridTable( $("#incubationreport_list"), true );
     };
-    
+
     // Incubation Lists
     var handleRecordsIncubationDataList = function() {
         gridTable( $("#list_incubation"), true );
     };
-    
+
     // Tenant Accompaniment Lists
     var handleRecordsTenantAccompanimentList = function() {
         gridTable( $("#tenantaccompaniment_list"), true );
     };
-    
+
     // Tenant Accepted Lists
     var handleRecordsTenantAcceptedList = function() {
         gridTable( $("#tenantacceptedselection_list"), true );
     };
     // -------------------------------------------------------------------------
-    
+
+    // -------------------------------------------------------------------------
+    // IKM
+    // -------------------------------------------------------------------------
+    // List IKM
+    var handleRecordsIKMList = function() {
+        gridTable( $("#list_ikm"), true );
+    };
+
     // -------------------------------------------------------------------------
     // NEWS
-    // ------------------------------------------------------------------------- 
-    // News List Admin 
+    // -------------------------------------------------------------------------
+    // News List Admin
     var handleRecordsNewsList = function() {
         gridTable( $("#news_list"), true );
     };
-    
+
     // -------------------------------------------------------------------------
     // SERVICES
-    // ------------------------------------------------------------------------- 
+    // -------------------------------------------------------------------------
     // List Communication
     var handleRecordsListIn = function() {
         gridTable( $("#communication_listin"), true );
     };
-    
+
     var handleRecordsListOut = function() {
         gridTable( $("#communication_listout"), true );
     };
-    
+
     // -------------------------------------------------------------------------
-    
+
     // -------------------------------------------------------------------------
     // TENANT
-    // ------------------------------------------------------------------------- 
+    // -------------------------------------------------------------------------
     // Incubation Selection Lists
     var handleRecordsTenantList = function() {
         gridTable( $("#list_tenant"), true );
     };
-    
+
     // Tenant Selection Lists
     var handleRecordsTenantSelectionList = function() {
         gridTable( $("#tenant_list"), true );
     };
-    
+
     // -------------------------------------------------------------------------
-    
+
     // Tenant Selection Lists
     var handleRecordsGuidesList = function() {
         gridTable( $("#guide_list"), true );
     };
-    
+
     // Juri Selection Lists Step One
     var handleRecordsJuryStepOneList = function() {
         gridTable( $("#jury_stepone"), true );
     };
-    
+
     // Juri Selection Lists Step Two
     var handleRecordsJuryStepTwoList = function() {
         gridTable( $("#jury_steptwo"), true );
     };
-    
-    // Workunit Lists 
+
+    // Workunit Lists
     var handleRecordsWorkunitList = function() {
         gridTable( $("#workunit_list"), true );
     };
-    
-    // Category Lists 
+
+    // Category Lists
     var handleRecordsCategoryList = function() {
         gridTable( $("#category_list"), true );
     };
-    
-    // Product Lists 
+
+    // Product Lists
     var handleRecordsProductList = function() {
         gridTable( $("#product_list"), true );
     };
-    
-    // Slider Lists 
+
+    // Slider Lists
     var handleRecordsSliderList = function() {
         gridTable( $("#slider_list"), true );
     };
-    
-    // General Message Lists 
+
+    // General Message Lists
     var handleRecordsGeneralMessageList = function() {
         gridTable( $("#generalmessage_list"), true );
     };
-    
-    // Announcement Lists 
+
+    // Announcement Lists
     var handleRecordsAnnouncementList = function() {
         gridTable( $("#announcement_list"), true );
     };
-    
-    // Announcement User Lists 
+
+    // Announcement User Lists
     var handleRecordsAnnouncementUserList = function() {
         gridTable( $("#announcementuser_list"), true );
     };
-    
+
     var gridTable = function(el, action=false, target='' ) {
         var url     = el.data('url');
         var grid    = new Datatable();
@@ -234,23 +242,23 @@ var TableAjax = function () {
 		       ]
             }
         });
-        
+
         if( action == true ){
             gridExport( grid, '.table-export-excel' );
         }
     }
-    
+
     var gridExport = function( dataTable, selectorBtn, sAction ) {
     	// handle group actionsubmit button click
         dataTable.getTableWrapper().on('click', selectorBtn, function(e) {
             e.preventDefault();
-            
+
             if ( typeof sAction == 'undefined' )
             	sAction = 'export_excel';
-        	
+
             dataTable.addAjaxParam( "sAction", sAction );
             var table = $( selectorBtn ).closest( '.table-container' ).find( 'table' );
-            
+
             // get all typeable inputs
             $( 'textarea.form-filter, select.form-filter, input.form-filter:not([type="radio"],[type="checkbox"])', table ).each( function() {
                 dataTable.addAjaxParam( $(this).attr("name"), $(this).val() );
@@ -260,7 +268,7 @@ var TableAjax = function () {
             $( 'input.form-filter[type="checkbox"]:checked, input.form-filter[type="radio"]:checked', table ).each( function() {
                 dataTable.addAjaxParam( $(this).attr("name"), $(this).val() );
             });
-            
+
             dataTable.getDataTable().fnDraw();
             dataTable.clearAjaxParams();
         });
@@ -272,7 +280,7 @@ var TableAjax = function () {
             initPickers();
             //User
             handleRecordsUserList();
-            
+
             //Pra Incubation
             handleRecordsPraIncubationSelectionList();
             handleRecordsPraIncubationSelectionList2();
@@ -287,47 +295,50 @@ var TableAjax = function () {
             handleRecordsPraIncubationAccompanimentList();
             handleRecordsPraIncubationAcceptedList();
             handleRecordsPraIncubationList();
-            
+
             //Incubation
             handleRecordsIncubationSelectionList();
             handleRecordsIncubationSelectionList2();
             handleRecordsIncubationSettingSelectionList();
             handleRecordsIncubationReportSelectionList();
             handleRecordsIncubationDataList();
-            
+
             //Tenant
             handleRecordsTenantList();
             handleRecordsTenantSelectionList();
             handleRecordsTenantAccompanimentList();
             handleRecordsTenantAcceptedList();
-            
+
             //Jury
             handleRecordsJuryStepOneList();
             handleRecordsJuryStepTwoList();
             handleRecordsJuryHistoryIncubationList();
-            
+
             //Product
             handleRecordsProductList();
-            
+
             //Workunit
             handleRecordsWorkunitList();
-            
+
             //Category
             handleRecordsCategoryList();
-            
+
             //Announcement
             handleRecordsAnnouncementList();
             handleRecordsAnnouncementUserList();
-            
+
             //Guide
             handleRecordsGuidesList();
-            
-            //News 
+
+            //IKM
+            handleRecordsIKMList();
+
+            //News
             handleRecordsNewsList();
-            
+
             //Slider
             handleRecordsSliderList();
-            
+
             //Service
             //Communication
             handleRecordsListIn();
