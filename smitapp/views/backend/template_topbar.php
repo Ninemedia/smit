b77e8124bc123e9d06123b13cb00214dd6c71fda
Muhamad Iqbal <muhammadiqbal1917@gmail.com>
2017-07-12 09:33:51 +0700

    Update Logo Backend

--- smitapp/views/backend/template_topbar.php
+++ smitapp/views/backend/template_topbar.php
@@ -4,8 +4,8 @@
         <div class="navbar-header">
             <a href="javascript:void(0);" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false"></a>
             <a href="javascript:void(0);" class="bars"></a>
-            <a class="navbar-brand" href="index.html">
-                <img src="<?php echo BE_IMG_PATH . 'logo/favicon_backup.png'; ?>" />
+            <a class="navbar-brand" href="<?php echo base_url('beranda'); ?>">
+                <img src="<?php echo BE_IMG_PATH . 'logo/logo-image.jpg'; ?>" />
                 <?php echo strtoupper(COMPANY_NAME); ?>
             </a>
         </div>
