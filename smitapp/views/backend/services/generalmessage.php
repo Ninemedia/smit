55d0624e07172a9772625ca9bc48c8bb6863716c
Raden Rifal Ardiansyah <rardiansyah@pukado.lan>
2017-07-19 09:34:30 +0700

    Update ceklist

--- smitapp/views/backend/services/generalmessage.php
+++ smitapp/views/backend/services/generalmessage.php
@@ -12,7 +12,7 @@
                         </a>
                     </li>
                 </ul>
-            
+
                 <!-- Tab panes -->
                 <div class="tab-content">
                     <div role="tabpanel" class="tab-pane fade in active" id="list">
@@ -21,9 +21,20 @@
                         </p>
                         <div id="alert" class="alert display-hide"></div>
                         <div class="table-container table-responsive">
+                            <div class="table-actions-wrapper">
+        						<span></span>
+        						<select class="table-group-action-input form-control input-inline input-small input-sm" disabled="disabled">
+        							<option value="">Select...</option>
+        							<option value="confirm">Konfirmasi</option>
+        							<option value="banned">Banned</option>
+        							<option value="delete">Hapus</option>
+        						</select>
+        						<button class="btn btn-sm btn-primary table-group-action-submit" disabled="disabled">Proses</button>
+        					</div>
                             <table class="table table-striped table-bordered table-hover" id="generalmessage_list" data-url="<?php echo base_url('backend/generalmessagelistdata'); ?>">
                                 <thead>
             						<tr role="row" class="heading bg-blue">
+                                        <th class="width5 text-center"><input name="select_all" id="select_all" value="1" type="checkbox" class="filled-in chk-col-orange" /><label for="select_all"></label></th>
             							<th class="width5">No</th>
             							<th class="width20 text-center">Nama Pengirim</th>
             							<th class="width15 text-center">Judul Pesan</th>
@@ -34,6 +45,7 @@
        						        </tr>
                                     <tr role="row" class="filter display-hide table-filter">
             							<td></td>
+                                        <td></td>
             							<td><input type="text" class="form-control form-filter input-sm text-uppercase" name="search_name" /></td>
             							<td><input type="text" class="form-control form-filter input-sm text-uppercase" name="search_title" /></td>
             							<td><input type="text" class="form-control form-filter input-sm text-lowercase" name="search_email" /></td>
@@ -65,7 +77,7 @@
                                 </tbody>
                             </table>
                         </div>
-                        
+
                     </div>
                 </div>
             </div>
